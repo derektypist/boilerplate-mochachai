992848ed97920c1ad09fd2e1a9210ee225950562
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-04 11:57:41 +0000

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -138,10 +138,17 @@ suite('Functional Tests', function() {
       // Try it again. This time without help !!
       test('send {surname: "da Verrazzano"}', function(done) {
         /** place the chai-http request code here... **/
-        
+        chai.request(server)
+          .put('/travellers')
+          .send({surname: 'da Verrazzano'})
         /** place your tests inside the callback **/
+        .end(function(err, res) {
+            assert.equal(res.status, 200, 'response status should be 200');
+            assert.equal(res.type, 'application/json', "Response should be JSON");
+            assert.equal(res.body.name, 'Giovanni', 'res.body.name should be "Giovanni"');
+            assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be "da Verrazzano"');
+          });
         
-        assert.fail(); // remove this after adding tests
         done();
       });
     });
